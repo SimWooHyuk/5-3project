c9f883e30edd1fdfe0910d11870bdbfcfa43910d
SimWooHyuk <tladngur2@naver.com>
2023-04-28 19:01:49 +0900

    4/28 금요일 로그인 회원가입

--- JS/main.js
+++ JS/main.js
@@ -5,7 +5,7 @@ thisYear.textContent = new Date().getFullYear();
 const spyEls = document.querySelectorAll('.scroll-spy');
 spyEls.forEach(function (spyEl) {
   new ScrollMagic
-    .Scene({
+    .   Scene({
       triggerElement: spyEl,
       triggerHook: 0.8
     })
@@ -28,6 +28,31 @@ function closePopup() {
 const toNewsEl = document.querySelector('.item__name3');
 toNewsEl.addEventListener('click', function (event) {
   event.preventDefault();
-  $('html,body').animate({ scrollTop: $(this.hash).offset().top - 100}, 500);
+  $('html,body').animate({ scrollTop: $(this.hash).offset().top - 150}, 500);
+});
+// 유튜브 위치 이동
+const toYTEl = document.querySelector('.item__name4');
+toYTEl.addEventListener('click', function (event) {
+  event.preventDefault();
+  $('html,body').animate({ scrollTop: $(this.hash).offset().top - 150}, 500);
+});
 
+//FADE IN WITH SILDE IN  // 몰라서 검색함 // 
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var delay = 500;
+    var i = 1;
+    $('.cl-menu').each(function() {
+      var $element = $(this);
+      var bottom_of_element = $element.offset().top + $element.outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      if (bottom_of_window > bottom_of_element) {
+        setTimeout(function() {
+          $element.animate({'opacity': '1', 'margin-left': '0px'}, 1000);
+        }, delay * i);
+        i++;
+      }
+    });
+  });
 });
